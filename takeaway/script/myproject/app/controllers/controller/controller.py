76e858c2e44f4d11591b603c82e2816af77c2840
sabuhish <sabuhi.shukurov@gmail.com>
2020-06-06 18:19:44 +0400

    fastapi seems to be ready but could not activate env 😑

--- takeaway/script/myproject/app/controllers/controller/controller.py
+++
@@ -1,4 +0,0 @@
-from fastapi import APIRouter, HTTPException,Cookie, Depends,Header,File, Body,Query
-from starlette.responses import JSONResponse
-from core.factories import settings
-import  httpx
--- takeaway/script/myproject/app/utils/helpers.py
+++
@@ -1,2 +0,0 @@
-def clean_dict(data):
-    return {key:val for (key,val) in data.items() if val is not None }
